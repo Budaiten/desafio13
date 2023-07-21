4a8df566cbe08ec17f71f74a6562b460f048bc2b
AlanAlvarez <fantamigosmg@gmail.com>
2023-07-21 07:36:26 -0300

    subiendo template nosotros

--- desafio13/urls.py
+++ desafio13/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index,nosotros
 from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('nosotros/',nosotros,name='nosotros'),
     path('', include('apps.noticias.urls')),
     path('', include('apps.contacto.urls')),
 ]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- desafio13/views.py
+++ desafio13/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def nosotros(request):
+
+    return render(request,'nosotros/nosotros.html')
